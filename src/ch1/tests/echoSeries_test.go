8007676249e645a813e4c29beb735336d2694347
Artem Melnyk <artem.melnyk@pm.me>
2020-05-21 22:01:55 +0300

    Dev: refactored so git test now understand me xD

--- src/ch1/tests/echoSeries_test.go
+++ src/ch1/tests/echoSeries_test.go
@@ -6,7 +6,6 @@ import (
 	"ch1/echoSeries/echo3"
 	"ch1/echoSeries/exercise1.1"
 	"ch1/echoSeries/exercise1.2"
-	"common/tests/std"
 	"testing"
 )
 
@@ -14,28 +13,28 @@ func TestPrint(t *testing.T) {
 	inArgs := []string{"", "Cool,", "it", "works!"}
 	expO, expE := "Cool, it works!\n", ""
 	t.Run("echo1", func(t *testing.T) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		echo1.Print()
-		std.TearDown(t, or, ow, er, ew, expO, expE)
+		TearDown(t, or, ow, er, ew, expO, expE)
 	})
 	t.Run("echo2", func(t *testing.T) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		echo2.Print()
-		std.TearDown(t, or, ow, er, ew, expO, expE)
+		TearDown(t, or, ow, er, ew, expO, expE)
 	})
 	t.Run("echo3", func(t *testing.T) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		echo3.Print()
-		std.TearDown(t, or, ow, er, ew, expO, expE)
+		TearDown(t, or, ow, er, ew, expO, expE)
 	})
 
 	// exercise 1
 	inArgs = []string{"exercise1", "Cool,", "it", "works!"}
 	expO, expE = "program: exercise1, args: Cool, it works!\n", ""
 	t.Run("exercise1", func(t *testing.T) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		exercise1_1.Print()
-		std.TearDown(t, or, ow, er, ew, expO, expE)
+		TearDown(t, or, ow, er, ew, expO, expE)
 	})
 
 	// exercise 2
@@ -45,9 +44,9 @@ func TestPrint(t *testing.T) {
 		"index: 2, value: it\n"+
 		"index: 3, value: works!\n", ""
 	t.Run("exercise2", func(t *testing.T) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		exercise1_2.Print()
-		std.TearDown(t, or, ow, er, ew, expO, expE)
+		TearDown(t, or, ow, er, ew, expO, expE)
 	})
 }
 
@@ -55,18 +54,18 @@ func BenchmarkEcho(b *testing.B) {
 	inArgs := []string{"", "Cool,", "it", "works!"}
 	expO, expE := "Cool, it works!\n", ""
 	b.Run("echo1", func(b *testing.B) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		echo1.Print()
-		std.TearDown(b, or, ow, er, ew, expO, expE)
+		TearDown(b, or, ow, er, ew, expO, expE)
 	})
 	b.Run("echo2", func(b *testing.B) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		echo2.Print()
-		std.TearDown(b, or, ow, er, ew, expO, expE)
+		TearDown(b, or, ow, er, ew, expO, expE)
 	})
 	b.Run("echo3", func(b *testing.B) {
-		or, ow, er, ew := std.SetUp(inArgs)
+		or, ow, er, ew := SetUp(inArgs)
 		echo3.Print()
-		std.TearDown(b, or, ow, er, ew, expO, expE)
+		TearDown(b, or, ow, er, ew, expO, expE)
 	})
 }
