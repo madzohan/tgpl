41c4229120d19fb55ce5948da5304558d90ae9cd
Artem Melnyk <artem.melnyk@pm.me>
2020-05-21 17:05:58 +0300

    New feature: #5 implemented dup2 with test coverage Dev: afero dependency (easy work with IO in tests)

--- src/ch1/dupSeries/tests/dup_test.go
+++ src/ch1/dupSeries/tests/dup_test.go
@@ -1,24 +1,56 @@
 package tests
 
 import (
-	"bytes"
 	"ch1/dupSeries/dup1"
+	"ch1/dupSeries/dup2"
 	"common/tests/std"
+	"github.com/spf13/afero"
+	"os"
+	"strings"
 	"testing"
 )
 
-func TestFindDuplicateLines(t *testing.T) {
-	stdin := bytes.NewBufferString("")
-	stdin.WriteString("same_line\n")
-	stdin.WriteString("yet_another_line\n")
-	stdin.WriteString("same_line\n")
-	stdin.WriteString("another_line\n")
-	stdin.WriteString("same_line\n")
-	expO := "same_line: 3"
+var (
+	FS     afero.Fs
+	FSUtil *afero.Afero
+)
 
+func init() {
+	FS = afero.NewMemMapFs()
+	FSUtil = &afero.Afero{Fs: FS}
+
+	_ = FSUtil.WriteFile("test1", []byte("same_line\nyet_another_line\nsame_line\nanother_line\n"), 0644)
+	_ = FSUtil.WriteFile("test2", []byte("same_line\n"), 0644)
+}
+
+func TestFindDuplicateLines(t *testing.T) {
+	stdin := "same_line\nyet_another_line\nsame_line\nanother_line\nsame_line\n"
+	expO := "line: same_line, count: 3"
+
+	stdinBuf := strings.NewReader(stdin)
 	t.Run("dup1", func(t *testing.T) {
 		or, ow, er, ew := std.SetUp([]string{})
-		dup1.FindDuplicateLines(stdin)
+		dup1.FindDuplicateLines(stdinBuf)
 		std.TearDown(t, or, ow, er, ew, expO, "")
+	})
+
+	stdinBuf = strings.NewReader(stdin)
+	t.Run("dup2-stdin", func(t *testing.T) {
+		or, ow, er, ew := std.SetUp([]string{"dup2-stdin"})
+		dup2.FindDuplicateLines(FS, stdinBuf)
+		std.TearDown(t, or, ow, er, ew, expO, "")
+	})
+
+	t.Run("dup2-files", func(t *testing.T) {
+		or, ow, er, ew := std.SetUp([]string{"dup2-files", "test1", "test2"})
+		dup2.FindDuplicateLines(FS, os.Stdin)
+		std.TearDown(t, or, ow, er, ew, expO, "")
+	})
+
+	t.Run("dup2-files-stderr", func(t *testing.T) {
+		or, ow, er, ew := std.SetUp([]string{"dup2-files", "file_not_exist"})
+		dup2.FindDuplicateLines(FS, os.Stdin)
+		std.TearDown(t, or, ow, er, ew, "",
+			"dup2: open file_not_exist: file does not exist")
 	})
 }
